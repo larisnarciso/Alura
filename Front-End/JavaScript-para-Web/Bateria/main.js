8adde9e6646b750fc1e5cdc643ce7634be5f7eac
Larissa Narciso <larisnarciso@gmail.com>
2023-12-27 19:58:19 -0300

    adicionando executar pelo teclado

--- Front-End/JavaScript-para-Web/Bateria/main.js
+++ Front-End/JavaScript-para-Web/Bateria/main.js
@@ -15,4 +15,21 @@ listaDeTeclas.forEach((tecla) => {
   tecla.onclick = function () {
     tocaSom(idAudio);
   };
+
+  // Ao navegar pelo teclado
+  tecla.onkeydown = function (event) {
+    // Se apertar Enter, Espaço ou o Enter ao lado do teclado númerico
+    if (
+      event.code === 'Enter' ||
+      event.code === 'Space' ||
+      event.code === 'NumpadEnter'
+    ) {
+      // Irá adicionar a classe 'ativa' para adicionar os efeitos definidos no css
+      tecla.classList.add('ativa');
+    }
+  };
+  // Quando soltar a tecla, irá retirar a classe 'ativa'
+  tecla.onkeyup = function () {
+    tecla.classList.remove('ativa');
+  };
 });

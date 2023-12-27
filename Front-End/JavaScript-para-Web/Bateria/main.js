6b096452a01d9af35528e076f2b43f80ec4b395e
Larissa Narciso <larisnarciso@gmail.com>
2023-12-27 19:08:58 -0300

    adicionando comentarios

--- Front-End/JavaScript-para-Web/Bateria/main.js
+++ Front-End/JavaScript-para-Web/Bateria/main.js
@@ -5,9 +5,13 @@ function tocaSom(idElementoAudio) {
 // Armazena todos os elementos com a classe 'tecla' em uma lista
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
+// Para cada elemento da lista
 listaDeTeclas.forEach((tecla) => {
+  // Extrai a segunda classe do elemento atual
   const instrumento = tecla.classList[1];
+  // Gera o ID do elemento correspondente
   const idAudio = `#som_${instrumento}`;
+  // Ao clicar em um botão, será reproduzido o som correspondente
   tecla.onclick = function () {
     tocaSom(idAudio);
   };

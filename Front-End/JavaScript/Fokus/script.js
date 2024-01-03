f20a6d0d1e851681c399f4e83e2687219e981e4a
Larissa Narciso <larisnarciso@gmail.com>
2024-01-03 15:25:38 -0300

    alterando botoes

--- Front-End/JavaScript/Fokus/script.js
+++ Front-End/JavaScript/Fokus/script.js
@@ -1,30 +1,31 @@
 const html = document.querySelector('html');
-// const listaBt = document.querySelectorAll('.app__card-button');
+const listaBt = document.querySelectorAll('.app__card-button');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
 
-const focoBt = document.querySelector('.app__card-button--foco');
-const curtoBt = document.querySelector('.app__card-button--curto');
-const longoBt = document.querySelector('.app__card-button--longo');
-
-focoBt.addEventListener('click', () => {
-  alterarContexto('foco');
-});
-
-curtoBt.addEventListener('click', () => {
-  alterarContexto('descanso-curto');
-});
-
-longoBt.addEventListener('click', () => {
-  alterarContexto('descanso-longo');
-});
-
-// listaBt.forEach((botao) => {
-//   botao.addEventListener('click', () => {
-//     const contexto = botao.dataset.contexto;
-//     alterarContexto(contexto);
-//   });
+// const focoBt = document.querySelector('.app__card-button--foco');
+// const curtoBt = document.querySelector('.app__card-button--curto');
+// const longoBt = document.querySelector('.app__card-button--longo');
+// focoBt.addEventListener('click', () => {
+//   alterarContexto('foco');
 // });
+// curtoBt.addEventListener('click', () => {
+//   alterarContexto('descanso-curto');
+// });
+// longoBt.addEventListener('click', () => {
+//   alterarContexto('descanso-longo');
+// });
+
+listaBt.forEach((botao) => {
+  botao.addEventListener('click', () => {
+    const contexto = botao.dataset.contexto;
+    alterarContexto(contexto);
+    botao.classList.add('active');
+    listaBt.forEach((outroBotao) => {
+      if (outroBotao !== botao) outroBotao.classList.remove('active');
+    });
+  });
+});
 
 function alterarContexto(contexto) {
   html.setAttribute('data-contexto', contexto);

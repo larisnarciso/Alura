f77d8519a47d7f7405087abdd36fdea0c50a44e0
Larissa Narciso <larisnarciso@gmail.com>
2024-01-05 14:02:01 -0300

    adicionando evento de adicionar tarefa por localStorage

--- Front-End/JavaScript/Fokus/script-crud.js
+++ Front-End/JavaScript/Fokus/script-crud.js
@@ -1,7 +1,18 @@
 // encontrar o botão adicionar tarefa
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
 const formAdicionarTarefa = document.querySelector('.app__form-add-task');
+const textarea = document.querySelector('.app__form-textarea');
+const tarefas = [];
 
 btnAdicionarTarefa.addEventListener('click', () => {
-  formAdicionarTarefa.classList.toggle('hidden');
+  formAdicionarTarefa.classList.toggle('hidden'); // mostrar/ocultar nova tarefa
+});
+
+formAdicionarTarefa.addEventListener('submit', (evento) => {
+  evento.preventDefault(); // evitar que paginas recarregue
+  const tarefa = {
+    descricao: textarea.value,
+  };
+  tarefas.push(tarefa); // coloca a tarefa dentro da lista de tarefas
+  localStorage.setItem('tarefas', JSON.stringify(tarefas));
 });

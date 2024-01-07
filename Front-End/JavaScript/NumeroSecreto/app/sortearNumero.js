0a7ccf0b814b1682944f26522a6cca5df0dac08d
Larissa Narciso <larisnarciso@gmail.com>
2024-01-07 14:06:15 -0300

    funcao math.random para gerar numero aleatorio

---
+++ Front-End/JavaScript/NumeroSecreto/app/sortearNumero.js
@@ -0,0 +1,21 @@
+// Selecionar elementos HTML
+const elementoMenorValor = document.querySelector('#menor-valor');
+const elementoMaiorValor = document.querySelector('#maior-valor');
+
+// Definir valores mínimo e máximo
+const menorValor = 1;
+const maiorValor = 100;
+
+// Atualizar elementos HTML com os valores mínimo e máximo
+elementoMenorValor.innerHTML = menorValor;
+elementoMaiorValor.innerHTML = maiorValor;
+
+// Gerar número aleatório entre o valor mínimo e máximo
+const numeroSecreto = gerarNumeroAleatorio();
+
+function gerarNumeroAleatorio() {
+  return parseInt(Math.random() * (maiorValor - menorValor + 1) + menorValor);
+}
+
+// Exibir o número secreto no console para teste
+console.log('numero secreto:', numeroSecreto);

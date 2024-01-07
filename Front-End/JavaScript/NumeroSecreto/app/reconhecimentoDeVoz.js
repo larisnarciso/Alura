b66b74fc2e2a4e69565e62c63b851d62adb598f7
Larissa Narciso <larisnarciso@gmail.com>
2024-01-07 15:53:11 -0300

    mostrando mensagem de voz na tela

--- Front-End/JavaScript/NumeroSecreto/app/reconhecimentoDeVoz.js
+++ Front-End/JavaScript/NumeroSecreto/app/reconhecimentoDeVoz.js
@@ -1,5 +1,19 @@
+const elementoChute = document.getElementById('chute');
 const SpeechRecognition = window.SpeechRecongnition || webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
 recognition.lang = 'pt-Br';
 recognition.start();
+
+recognition.addEventListener('result', onSpeak);
+
+function onSpeak(e) {
+  chute = e.results[0][0].transcript;
+  exibeChuteNaTela(chute);
+}
+
+function exibeChuteNaTela(chute) {
+  elementoChute.innerHTML = `
+  <div>Você disse</div>
+  <span class='box'> ${chute}</span>`;
+}

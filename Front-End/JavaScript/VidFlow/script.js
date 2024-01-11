043634b150e2fbfb55d37b38219be6cb44803ce5
Larissa Narciso <larisnarciso@gmail.com>
2024-01-11 18:15:49 -0300

    exibindo videos na aplicacao

--- Front-End/JavaScript/VidFlow/script.js
+++ Front-End/JavaScript/VidFlow/script.js
@@ -1,6 +1,21 @@
+const containerVideos = document.querySelector('.videos__container');
+
 /* A constante API está sendo atribuída à chamada da função fetch, que realiza uma requisição para a URL 'http://localhost:3000/videos'
 e retorna uma Promise. O método then() é encadeado para lidar com a resposta da requisição, convertendo-a para JSON através
 do método response.json(). Assim, a constante API representa a Promise resultante da requisição assíncrona para obter dados de vídeos. */
 const API = fetch('http://localhost:3000/videos').then((response) =>
-  response.json()
+  response.json().then((videos) => {
+    videos.forEach((video) => {
+      containerVideos.innerHTML += `
+      <li class='videos__item'>
+        <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+        <div class='descricao-video'>
+          <img class='img-canal' src='${video.imagem} alt='Logo do Canal'>
+          <h3 class='titulo-video'>${video.titulo}</h3>
+          <p class='titulo-canal'>${video.descricao}<p/>
+        </div>
+      </li>
+      `;
+    });
+  })
 );

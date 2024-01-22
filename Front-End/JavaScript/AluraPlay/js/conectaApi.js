dcf416c0b7393faa755277a4b19da913f597d8d3
Larissa Narciso <larisnarciso@gmail.com>
2024-01-22 16:25:09 -0300

    Requisicao POST

--- Front-End/JavaScript/AluraPlay/js/conectaApi.js
+++ Front-End/JavaScript/AluraPlay/js/conectaApi.js
@@ -1,15 +1,31 @@
 // Endpoint da API
 const endpointAPI = 'http://localhost:3000/videos';
 
+// Função assíncrona para obter a lista de vídeos através de uma requisição GET
 async function listaVideos() {
-  // Função assíncrona para buscar os videos da API
-  const conexao = await fetch(endpointAPI); // Requisição
-  const conexaoConvertida = await conexao.json(); // Converte para o formato JSON
+  const conexao = await fetch(endpointAPI); // Realiza a requisição
+  const conexaoConvertida = await conexao.json(); // Converte a resposta para o formato JSON
   return conexaoConvertida;
 }
 
-listaVideos();
+// Função assíncrona para criar um novo vídeo através de uma requisição POST
+async function criaVideo(titulo, descricao, url, imagem) {
+  const conexao = await fetch(endpointAPI, {
+    method: 'POST',
+    headers: { 'Content-type': 'application/json' },
+    body: JSON.stringify({
+      titulo: titulo,
+      descricao: `${descricao} mil visualizações`,
+      url: url,
+      imagem: imagem,
+    }),
+  });
+  const conexaoConvertida = await conexao.json();
+  return conexaoConvertida;
+}
 
+// Objeto que exporta as funções para conectar à API
 export const conectaApi = {
   listaVideos,
+  criaVideo,
 };

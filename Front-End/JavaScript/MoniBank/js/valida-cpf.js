0d694dccbc498d58bba294caf3c910a5b4b3eb27
Larissa Narciso <larisnarciso@gmail.com>
2024-01-25 18:28:44 -0300

    verifica se cpf existe

--- Front-End/JavaScript/MoniBank/js/valida-cpf.js
+++ Front-End/JavaScript/MoniBank/js/valida-cpf.js
@@ -5,7 +5,13 @@
 export default function ehUmCPF(campo) {
   // Remover pontos e traços do valor do CPF para facilitar a validação
   const cpf = campo.value.replace(/\.|-/g, '');
-  validaNumerosRepetidos(cpf); // Chama a função auxiliar para verificar se há números repetidos
+  if (
+    validaNumerosRepetidos(cpf) ||
+    validaPrimeiroDigito(cpf) ||
+    validaSegundoDigito(cpf)
+  )
+    console.log('Esse CPF não existe');
+  else console.log('Esse CPF existe');
 }
 
 /**
@@ -27,4 +33,44 @@ function validaNumerosRepetidos(cpf) {
     '99999999999',
   ];
   return numeroRepetidos.includes(cpf); // Verifica se o CPF está na lista de números repetidos
+}
+
+/**
+ * Função auxiliar que valida o primeiro dígito do CPF.
+ * @param {string} cpf - O número de CPF sem pontos e traços.
+ * @returns {boolean} - Retorna true se o primeiro dígito for válido, caso contrário, retorna false.
+ */
+function validaPrimeiroDigito(cpf) {
+  let soma = 0;
+  let multiplicador = 10;
+
+  for (let tamanho = 0; tamanho < 9; tamanho++) {
+    soma += cpf[tamanho] * multiplicador;
+    multiplicador--;
+  }
+
+  soma = (soma * 10) % 11;
+  if (soma == 10 || soma == 11) soma = 0;
+
+  return soma != cpf[9];
+}
+
+/**
+ * Função auxiliar que valida o segundo dígito do CPF.
+ * @param {string} cpf - O número de CPF sem pontos e traços.
+ * @returns {boolean} - Retorna true se o segundo dígito for válido, caso contrário, retorna false.
+ */
+function validaSegundoDigito(cpf) {
+  let soma = 0;
+  let multiplicador = 11;
+
+  for (let tamanho = 0; tamanho < 10; tamanho++) {
+    soma += cpf[tamanho] * multiplicador;
+    multiplicador--;
+  }
+
+  soma = (soma * 10) % 11;
+  if (soma == 10 || soma == 11) soma = 0;
+
+  return soma != cpf[10];
 }

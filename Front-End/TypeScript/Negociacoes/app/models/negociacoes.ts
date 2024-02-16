2f8c44601c2771c0bd168bcfb1800bca95c2694a
Larissa Narciso <larisnarciso@gmail.com>
2024-02-16 19:50:04 -0300

    comentarios com alternativas

--- Front-End/TypeScript/Negociacoes/app/models/negociacoes.ts
+++ Front-End/TypeScript/Negociacoes/app/models/negociacoes.ts
@@ -2,12 +2,14 @@ import { Negociacao } from './negociacao.js';
 
 export class Negociacoes {
   private negociacoes: Array<Negociacao> = [];
+  // private negociacoes: Negociacao[] = []; // alternativa
 
   adiciona(negociacao: Negociacao) {
     this.negociacoes.push(negociacao);
   }
 
   lista(): ReadonlyArray<Negociacao> {
+    // lista(): readonly Negociacao[]{ // alternativa
     return this.negociacoes;
   }
 }
--- Front-End/TypeScript/Negociacoes/dist/js/models/negociacoes.js
+++ Front-End/TypeScript/Negociacoes/dist/js/models/negociacoes.js
@@ -2,10 +2,12 @@ export class Negociacoes {
     constructor() {
         this.negociacoes = [];
     }
+    // private negociacoes: Negociacao[] = []; // alternativa
     adiciona(negociacao) {
         this.negociacoes.push(negociacao);
     }
     lista() {
+        // lista(): readonly Negociacao[]{ // alternativa
         return this.negociacoes;
     }
 }

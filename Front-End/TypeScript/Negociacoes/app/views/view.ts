635667b5cb01ecc8f242b309c8df0017b664145b
Larissa Narciso <larisnarciso@gmail.com>
2024-02-20 13:47:53 -0300

    protegendo template utilizando parametros opcionais

--- Front-End/TypeScript/Negociacoes/app/views/view.ts
+++ Front-End/TypeScript/Negociacoes/app/views/view.ts
@@ -1,12 +1,21 @@
 export abstract class View<T> {
   protected elemento: HTMLElement;
-  constructor(seletor: string) {
-    this.elemento = document.querySelector(seletor);
-  }
-  protected abstract template(model: T): string;
+  private escapar = false;
 
-  update(model: T): void {
-    const template = this.template(model);
+  constructor(seletor: string, escapar?: boolean) {
+    this.elemento = document.querySelector(seletor);
+    if (escapar) {
+      this.escapar = escapar;
+    }
+  }
+
+  public update(model: T): void {
+    let template = this.template(model);
+    if (this.escapar) {
+      template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+    }
     this.elemento.innerHTML = template;
   }
+
+  protected abstract template(model: T): string;
 }

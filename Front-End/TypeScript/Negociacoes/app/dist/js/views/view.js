8334bdf0eef2597d7b427a365fad83202145cf19
Larissa Narciso <larisnarciso@gmail.com>
2024-02-23 13:32:38 -0300

    decorator escapar

--- Front-End/TypeScript/Negociacoes/app/dist/js/views/view.js
+++ Front-End/TypeScript/Negociacoes/app/dist/js/views/view.js
@@ -7,21 +7,15 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 import { inspect } from '../decorators/inspect.js';
 import { logarTempoDeExecucao } from '../decorators/logar-tempo-de-execucao.js';
 export class View {
-    constructor(seletor, escapar) {
-        this.escapar = false;
+    constructor(seletor) {
         const elemento = document.querySelector(seletor);
         if (elemento)
             this.elemento = elemento;
         else
             throw Error(`Seletor ${seletor} não existe no DOM. Verifique`);
-        if (escapar)
-            this.escapar = escapar;
     }
     update(model) {
         let template = this.template(model);
-        if (this.escapar) {
-            template = template.replace(/<script>[\s\S]*?<\/script>/, '');
-        }
         this.elemento.innerHTML = template;
     }
 }

983fbf2412d896c2bd576041bcab3e41b86d7436
Larissa <larisnarciso@gmail.com>
2024-04-01 23:51:27 -0300

    componetizando secao sua lista

--- Front-End/Vue/CookinUp/src/components/ConteudoPrincipal.vue
+++ Front-End/Vue/CookinUp/src/components/ConteudoPrincipal.vue
@@ -1,6 +1,6 @@
 <script lang="ts">
 import SelecionarIngredientes from './SelecionarIngredientes.vue';
-import Tag from './Tag.vue';
+import SuaLista from './SuaLista.vue';
 
 export default {
   data() {
@@ -9,31 +9,14 @@ export default {
       // ingredientes: [], // testando lista vazia
     };
   },
-  components: { SelecionarIngredientes, Tag },
+  components: { SelecionarIngredientes, SuaLista },
 };
 </script>
 
 <
 <template>
   <main class="conteudo-principal">
-    <section>
-      <span class="subtitulo-lg sua-lista-texto">Sua lista:</span>
-
-      <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
-        <li v-for="ingrediente in ingredientes" :key="ingrediente">
-          <Tag :texto="ingrediente" ativa />
-        </li>
-      </ul>
-
-      <p v-else class="paragrafo lista-vazia">
-        <img
-          src="../assets/imagens/icones/lista-vazia.svg"
-          alt="Ícone de pesquisa"
-        />
-        Sua lista está vazia, selecione ingredientes para iniciar.
-      </p>
-    </section>
-
+    <SuaLista :ingredientes="ingredientes" />
     <SelecionarIngredientes />
   </main>
 </template>
@@ -49,31 +32,6 @@ export default {
   flex-direction: column;
   align-items: center;
   gap: 5rem;
-}
-
-.sua-lista-texto {
-  color: var(--coral, #f0633c);
-  display: block;
-  text-align: center;
-  margin-bottom: 1.5rem;
-}
-
-.ingredientes-sua-lista {
-  display: flex;
-  justify-content: center;
-  gap: 1rem 1.5rem;
-  flex-wrap: wrap;
-}
-
-.lista-vazia {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-wrap: wrap;
-  gap: 0.25rem;
-
-  color: var(--coral, #f0633c);
-  text-align: center;
 }
 
 @media only screen and (max-width: 1300px) {

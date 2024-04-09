ee2de3e52f3cfb2591a3a7a6a61fde39c4ab5b2a
Larissa <larissanarciso@opus127.com.br>
2024-04-08 22:06:33 -0300

    botao de buscar receitas e footer

--- Front-End/Vue/CookinUp/src/App.vue
+++ Front-End/Vue/CookinUp/src/App.vue
@@ -1,13 +1,15 @@
 <script lang="ts">
 import Banner from './components/Banner.vue';
 import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
+import Footer from './components/Footer.vue';
 
 export default {
-  components: { Banner, ConteudoPrincipal },
+  components: { Banner, ConteudoPrincipal, Footer },
 };
 </script>
 
 <template>
   <Banner />
   <ConteudoPrincipal />
+  <Footer />
 </template>
---
+++ Front-End/Vue/CookinUp/src/components/BotaoPrincipal.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+export default {
+  props: {
+    texto: { type: String, required: true },
+  },
+};
+</script>
+
+<template>
+  <button class="btn-principal paragrafo-lg">{{ texto }}</button>
+</template>
+
+<style scoped>
+.btn-principal {
+  box-shadow: 4px 4px 15px 0px rgba(255, 155, 76, 0.25);
+  background-color: var(--coral);
+  color: var(--creme);
+  font-weight: 700;
+  padding: 0.5rem 1rem;
+  border-radius: 2rem;
+  width: 19.5rem;
+  height: 3.5rem;
+  gap: 10px;
+  transition: 0.2s;
+  cursor: pointer;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.btn-principal:hover {
+  background: var(--ocre);
+}
+</style>
